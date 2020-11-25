86717d21e4cf5784642d0067c12d08b16f22d1f3
likaixuan <1375502718@qq.com>
2020-11-25 19:04:14 +0800

    fix:修复记录位置开关显示问题

--- miniprogram/pages/upload/index.js
+++ miniprogram/pages/upload/index.js
@@ -19,9 +19,16 @@ Page({
     fileList: [],
     uploadedList:[],
     content:'',
-    isLocationAuthorize:globalData.isLocationAuthorize,
-    isLocation:!globalData.isLocationAuthorize,
+    isLocationAuthorize:true,
+    isLocation:false,
     location:[]
+  },
+  onShow: function () {
+    const isLocationAuthorize = globalData.isLocationAuthorize
+    this.setData({
+      isLocationAuthorize,
+      isLocation:!isLocationAuthorize
+    })
   },
   onChange({ detail }) {
     console.log(detail,3434)
@@ -137,54 +144,6 @@ Page({
     } else {
       wx.showToast({ title: '等待照片上传完毕！', icon: 'none' });
     }
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
   },
 
   /**
