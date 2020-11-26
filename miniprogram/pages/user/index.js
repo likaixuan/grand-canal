6606a9b3ae48979b6cb5bda7f72f2fb0a559b5c6
likaixuan <1375502718@qq.com>
2020-11-26 11:37:54 +0800

    feat:分享相关

--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -1,7 +1,14 @@
 // pages/user/user.js
+const {
+  getShareInfo,
+  qrcodeHost
+} = require('../../shareConfig.js')
+
+console.log(getShareInfo(),3333)
 import {
   User
 } from '../../model/User.js'
+import { promisify } from '../../utils/util.js'
 const user = new User()
 const app = getApp()
 Page({
@@ -17,8 +24,32 @@ Page({
     isShowShare: false,
     options: [
       { name: '微信', icon: 'wechat', openType: 'share' },
-      { name: '分享海报', icon: 'poster' }
+      { name: '分享海报', icon: 'poster',code:'poster' }
     ],
+    originPosterOptionList: [{
+      id: 'poster1',
+      bg: {
+        src: '/static/img/share1.png',
+        x: 0,
+        y: 0,
+        height: 1008,
+        width: 640
+      },
+      avatar:{
+        defaultPath:'/static/img/avatar.png',
+        x:52,
+        y:42,
+        width:120,
+        height:120
+      },
+      qrCode: {
+        x: 526,
+        y: 892,
+        width: 88,
+        height: 88
+      }
+    }],
+    tempPosterPathList: []
   },
   /**
    * 生命周期函数--监听页面显示
@@ -31,12 +62,100 @@ Page({
     }
     this.initData()
   },
-  onShareAppMessage() {
-
+  onShareAppMessage: function() {
+    return {
+      ...getShareInfo()
+    }
   },
   /**
    * Methods 相关
    */
+  // 创建海报
+  generatePoster() {
+    if (this.data.tempPosterPathList.length > 0) {
+      wx.previewImage({
+        urls: this.data.tempPosterPathList
+      })
+      return
+    }
+    wx.showLoading({
+      title: '正在制作海报',
+      icon: 'loading',
+      mask: true
+    })
+    return new Promise((resolve,reject)=>{
+      if(app.globalData.userInfo.avatarUrl) {
+        return promisify(wx.getImageInfo)({
+          src:app.globalData.userInfo.avatarUrl.replace('http://thirdwx.qlogo.cn', 'https://wx.qlogo.cn')
+        }).then((res)=>{
+          resolve(res.path)
+          console.log(res,77777)
+        }).catch((err)=>{
+          reject(err)
+        })
+      } else {
+        resolve('/static/img/avatar.png')
+      }
+    }).then((avatar)=>{
+      const drawTask = this.data.originPosterOptionList.map((item) => {
+        // 将绘制的海报添加到并行队列中
+        return new Promise((resolve, reject) => {
+  
+          const posterContext = wx.createCanvasContext(item.id)
+          posterContext.drawImage(item.bg.src, item.bg.x, item.bg.y, item.bg.width, item.bg.height)
+          posterContext.save()
+          posterContext.beginPath()
+          // 画一个圆形裁剪区域
+          posterContext.arc(item.avatar.width / 2 + item.avatar.x, item.avatar.height / 2 + item.avatar.y, item.avatar.width / 2, 0, Math.PI * 2, false)
+          // 裁剪
+          posterContext.clip()
+          // 绘制背景图
+          posterContext.drawImage(avatar, item.avatar.x, item.avatar.y, item.avatar.width, item.avatar.height)
+          posterContext.restore()
+          posterContext.draw(false, (msg) => {
+            // 二维码绘制到海报上
+            wx.canvasToTempFilePath({
+              width: item.bg.width,
+              height: item.bg.height,
+              destWidth: item.bg.width,
+              destHeight: item.bg.height,
+              canvasId: item.id,
+              success: (res) => {
+                resolve(res.tempFilePath)
+              },
+              fail: (msg) => {
+                reject()
+              }
+            })
+          })
+        })
+      })
+  
+      return Promise.all(drawTask).then((tempPosterPathList) => {
+        wx.hideLoading()
+          console.log(tempPosterPathList,343434)
+          this.setData({
+            tempPosterPathList
+          })
+          wx.showModal({
+            title: '海报生成成功',
+            content: '请选择一张海报长按保存发送到朋友圈',
+            showCancel: false,
+            success: () => {
+              wx.previewImage({
+                urls: tempPosterPathList
+              })
+            }
+          })
+        })
+    }).catch(()=>{
+      wx.showToast({
+        title: '制作海报失败',
+        icon: 'none'
+      })
+    })
+    
+  },
   showPay() {
     wx.previewImage({
       urls: ['cloud://yegoudaxue-5g7j3z4r4142a6c6.7965-yegoudaxue-5g7j3z4r4142a6c6-1304300501/cdn/pay.png'] // 需要预览的图片http链接列表
@@ -48,16 +167,20 @@ Page({
     })
   },
   onCloseShare() {
+    // 关闭分享面板
     this.setData({ isShowShare: false });
   },
-
+  // 分享面板中选择分享项
   onSelectShare(event) {
+    // 选择生成海报
+    if(event.detail.code === 'poster') {
+      this.generatePoster()
+    }
     console.log(event,434545)
     this.onCloseShare()
   },
-  // 打开分享
+  // 打开分享面板
   openShare() {
-    console.log(6666)
     this.setData({
       isShowShare:true
     })
---
+++ miniprogram/shareConfig.js
@@ -0,0 +1,24 @@
+const globalData = getApp().globalData
+
+const shareList = [{
+  title: '发扬运河文化，传承感恩美德',
+  path: `/pages/index/index`,
+  imageUrl: '/static/img/share.png'
+}]
+
+// 获取分享信息
+let getShareInfo = function () {
+  const index = parseInt(Math.random() * shareList.length, 10)
+  // 所有转发携带userId、及其share 用于区分进入场景
+  shareList[index].path += `?referrerId=${globalData.userInfo._id}`
+  return shareList[index]
+}
+
+// 分享二维码host
+let qrcodeHost = ''
+
+
+export {
+  qrcodeHost,
+  getShareInfo
+}
